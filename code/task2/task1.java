0868689ed6a4b5d2b91f4fcf0b501d056f42024e
HyrumHansen <89932049+HyrumHansen@users.noreply.github.com>
2021-09-13 20:15:12 -0600

    Add files via upload

---
+++ code/task2/task1.java
@@ -0,0 +1,9 @@
+public class task1 {
+    public static void main(String[] args) {
+        printScreen();
+    }
+
+    public static void printScreen(){
+        System.out.println("That's no ordinary rabbit. That's the most foul, cruel, and bad-tempered rodent you ever set eyes on.");
+    }
+}
